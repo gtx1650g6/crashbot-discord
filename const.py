801796bed31db90ef271d0d06cec4a9563fcfd05
patrolet <156691160+gtx1650g6@users.noreply.github.com>
2024-04-29 19:58:47 +0300

    Update const.py

--- const.py
+++ const.py
@@ -1,11 +1,11 @@
-# Вставьте токен бота
-TOKEN = 'MTIyNjU2NzcwNDM1NDIyNjI2MA.GTAtfN.MPFj4Ga56XQguTiiSeUjHoPEHr4Dr5Sy_fjCSg'
+# Вставьте токен бота / Enter your bot token
+TOKEN = ''
 
-# Напишите спам текст для краша
+# Напишите спам текст для краша / Write text for crash
 TEXT = '# @everyone Ливайте с помой :-1:'
 
-# Количество спам сообщений в текстовые каналы
+# Количество спам сообщений в текстовые каналы / Wrtie amount of spam text in text channels
 MESSAGE_COUNT = 50
 
-# Количество спам каналов и ролей для создания
+# Количество спам каналов и ролей для создания / Write amount of spam channels and roles
 CHANNEL_AND_ROLE_COUNT = 50
